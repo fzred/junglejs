2386e2d1cc046a938d73f03e404803f4a6d9b08c
fzred <1179750402@qq.com>
2018-05-24 00:03:06 +0800

    init

---
+++ debug.js
@@ -0,0 +1,4 @@
+// 加载babel预编译
+require('babel-core/register')
+require("babel-core").transform()
+require('./src')
---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+import lexer from './lexer'
+
+setTimeout(() => {
+  const hello = 'jungle'
+  lexer()
+  console.log(hello)
+}, 1000)
